6fdea76cc5d60eb2fd404b37e31b6489aa32b16b
shumaz-saeed <147626403+shumaz-saeed@users.noreply.github.com>
2025-07-25 14:15:17 +0500

    Update demo.py

--- PYWHATKIT/demo.py
+++ PYWHATKIT/demo.py
@@ -4,4 +4,4 @@ import time
 
 
 # Send a WhatsApp message to a specific number
-kit.sendwhatmsg("+923357744304", "Hello from PyWhatKit!", 15, 0)
+kit.sendwhatmsg("phone number", "Hello from PyWhatKit!", 15, 0)
